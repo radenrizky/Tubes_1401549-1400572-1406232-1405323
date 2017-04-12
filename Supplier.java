3b36dd3b3be0423d6f1579ca36ed80accbae9664
radenrizky <radenrizky27@gmail.com>
2017-04-12 23:09:31 +0700

    Update Supplier.java

--- Supplier.java
+++ Supplier.java
@@ -1,5 +1,39 @@
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
- * Created by Raden Rizky Irianti on 12/04/2017.
+ * Created by User on 4/12/2017.
  */
 public class Supplier {
+    String kd_supplier;
+    String nama_supplier;
+    String no_telp;
+
+    HashMap<String,Supplier> hmSupplier = new HashMap<>();
+
+
+    public Suplier(String kd_supplier, String nama_supplier, String no_telp) {
+        this.kd_supplier = kd_supplier;
+        this.nama_supplier = nama_supplier;
+        this.no_telp = no_telp;
+    }
+
+
+    public void addSupplier(Supplier s) {
+        hmSupplier.put(s.kd_supplier, s);
+    }
+
+
+
+    public void hapusSupplier(String kd_hapus){
+        hmSupplier.remove(kd_hapus);
+    }
+
+    public static void main(String[] args) {
+        Supplier s = new Supplier("S105","Maharani", "081223452677");
+        s.addSupplier(s);
+        s.hapusSupplier("S105");
+    }
+
 }
